5c46dd5876fa168d71efbb4c29037fb3e26e89b1
Enes Turan <enestrn@gmail.com>
2024-03-19 12:26:14 +0300

    loot save hariç var

--- Assets/Scripts/Loot/LootManager.cs
+++ Assets/Scripts/Loot/LootManager.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 using Utils;
 using UnityEngine.UI;
 using TMPro;
-using UnityEngine.Rendering;
 using Utils.PoolSystem;
 using Unity.Mathematics;
 using DG.Tweening;
@@ -12,8 +11,8 @@ public class LootManager : Singleton<LootManager>
 {
     public GameData data;
     public GamePresets gamePresets;
-    public GameObject lootPanel, chestOpen, chestClose, step1, step2, deck, spawnedObj;
-    public Transform spawnPos, moveToPos, tempObj;
+    public GameObject lootPanel, chestOpen, chestClose, step1, step2, step3, deck, spawnedObj;
+    public Transform spawnPos, moveToPos, tempObj, gridView;
     public TMP_Text deckCount;
     public int remainingLootCount;
     public List<LootItem> currentLoot = new();
@@ -34,6 +33,7 @@ public class LootManager : Singleton<LootManager>
 
     public void NewLoot()
     {
+        gridView.gameObject.SetActive(false);
         lootPanel.SetActive(true);
         TaptoCollect(false);
         currentLoot = gamePresets.LootList[data.lootCount].loot;
@@ -42,7 +42,20 @@ public class LootManager : Singleton<LootManager>
     }
     public void Skip()
     {
-
+        gridView.gameObject.SetActive(true);
+        step1.SetActive(false);
+        step2.SetActive(false);
+        chestClose.transform.parent.gameObject.SetActive(false);
+        deck.SetActive(false);
+        for (int i = 0; i < spawnedLootItems.Count; i++)
+        {
+            var obj = spawnedLootItems[i];
+            obj.gameObject.SetActive(true);
+            obj.SetParent(gridView);
+            var rectTransform = obj.GetComponent<RectTransform>();
+            rectTransform.sizeDelta = new Vector2(200, 200);
+        }
+        step3.SetActive(true);
     }
     public void TaptoCollect(bool open)
     {
@@ -56,9 +69,17 @@ public class LootManager : Singleton<LootManager>
     }
     public void TapForNextItem()
     {
-        if (remainingLootCount <= 0) return;
+        if (remainingLootCount <= 0) { Skip(); return; }
         tempObj.gameObject.SetActive(false);
         SpawnLootObject();
+    }
+    public void Claim()
+    {
+        for (int i = 0; i < currentLoot.Count; i++)
+        {
+
+        }
+        lootPanel.SetActive(false);
     }
     private void ClearList()
     {
@@ -71,7 +92,6 @@ public class LootManager : Singleton<LootManager>
         // spawnedLootItems.Clear();
         if (spawnedLootItems.Count > 0)
             ClearList();
-
     }
     private void SpawnLootObject()
     {
